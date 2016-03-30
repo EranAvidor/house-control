6b62919ad1c5012a2af711c44de066b37e6366fb
EranAvidor <eranavidor89@gmail.com>
2016-03-30 22:33:26 +0300

    added /led routes

---
+++ routes/led.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var Gpio = require('onoff').Gpio,
+  greenLed = new Gpio(17, 'out'),
+  blueLed = new Gpio(18, 'out');
+  // button = new Gpio(4, 'in', 'both');
+
+/* GET home page. */
+router.get('/on', function(req, res, next) {
+  greenLed.writeSync(1);
+  res.render('okay');
+});
+router.get('/off', function(req, res, next) {
+  greenLed.writeSync(0);
+  res.render('okay');
+});
+
+module.exports = router;
